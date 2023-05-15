eb48b378af4f6361908f81fdde5401ac6a56f09f
Kirk Hauck <117328793+kirkhauck@users.noreply.github.com>
2023-05-15 11:35:36 -0600

    fix: Comment out useState import to satisfy lint Co-authored-by: KatherineBlaine <blain064@umn.edu> Co-authored-by: bea-ordonez <beaordonez25@gmail.com>
    Signed-off-by: Kirk Hauck <kirkihauck@gmail.com>

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 import Header from '../Header/Header';
 import Home from '../Home/Home';
 import Dashboard from '../Dashboard/Dashboard';
