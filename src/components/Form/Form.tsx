316eb8cd6e7d41143aeb2cd9b52f70304e45ab74
Kirk Hauck <kirkihauck@gmail.com>
2023-05-15 16:55:16 -0600

    feat: Setup state and component mount effect

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Form.css';
 import infoIcon from '../../assets/info_icon.svg';
 
@@ -9,23 +9,38 @@ import mockPrompt from '../../sampleData/prompt';
 const mockData = mockPrompt.data.attributes;
 const mockGrammar = mockData.grammar_points;
 
+interface IGrammarPoint {
+  grammar_point: string;
+  eng_grammar_point: string;
+}
+
 const Form = () => {
-  const [imgUrl, setImgUrl] = useState('');
-  const [imgAlt, setimgAlt] = useState('')
-  const [verb, setVerb] = useState('');
-  const [grammarPoints, setGrammarPoints] = useState([]);
-  const [sentences, setSentences] = useState([])
+  const [imgUrl, setImgUrl] = useState<string>('');
+  const [imgAlt, setImgAlt] = useState<string>('')
+  const [verb, setVerb] = useState<string>('');
+  const [engVerb, setEngVerb] = useState<string>('');
+  const [grammarPoints, setGrammarPoints] = useState<IGrammarPoint[]>([]);
+  const [sentences, setSentences] = useState<string[]>([]);
+
+  useEffect(() => {
+    //fetch new challenge someday
+    setImgUrl(mockData.image_url);
+    setImgAlt(mockData.image_alt_text);
+    setVerb(mockData.verb);
+    setEngVerb(mockData.eng_verb);
+    setGrammarPoints(mockGrammar);
+  }, [])
 
   return (
     <form>
-      <img alt="frog cat" src="https://cdn.shopify.com/s/files/1/0344/6469/products/froggyhat2_800x.jpg?v=1634330683"/>
+      <img alt={imgAlt} src={imgUrl}/>
       <div>
-        <h2>{mockData.verb}</h2>
-        <h3>{mockData.eng_verb}</h3>
+        <h2>{verb}</h2>
+        <h3>{engVerb}</h3>
         <img src={infoIcon} alt='instructions icon' />
-        <label>{mockGrammar[0].grammar_point} | {mockGrammar[0].eng_grammar_point}</label>
+        {grammarPoints.length && <label>{grammarPoints[0].grammar_point} | {grammarPoints[0].eng_grammar_point}</label>}
         <input type='text' />
-        <label>{mockGrammar[1].grammar_point} | {mockGrammar[1].eng_grammar_point}</label>
+        {grammarPoints.length && <label>{grammarPoints[1].grammar_point} | {grammarPoints[1].eng_grammar_point}</label>}
         <input type='text' />
       </div>
     </form>
--- src/sampleData/prompt.tsx
+++ src/sampleData/prompt.tsx
@@ -7,8 +7,8 @@ const mockPrompt = {
         language: 'Turkish', 
         verb: '(i) gitmek',
         eng_verb: 'to go',
-        image_url: '/random/unplash/image.url',
-        image_alt_text: 'Plane flying over the Bosphorous',
+        image_url: 'https://images.unsplash.com/photo-1573865526739-10659fec78a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1015&q=80',
+        image_alt_text: 'selective focus photography of orange and white cat on brown table',
         created_at: null,
         grammar_points: [
            {
