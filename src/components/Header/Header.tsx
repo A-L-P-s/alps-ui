5cc0d2beac17e08821017ac2aadb2d85501513de
Bea Ordonez <116661385+bea-ordonez@users.noreply.github.com>
2023-05-15 11:59:45 -0600

    Feature: Add  basic header component Co-authored-by: kirkhauck <kirkihauck@gmail.com> Co-authored-by: KatherineBlaine <blain064@umn.edu>
    Signed-off-by: Barbie Ordonez <ordonezbarbie@gmail.com>

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,16 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import React from 'react';
 import './Header.css';
+
+// interface IProps {
+//   name: string
+// }
 
 const Header = () => {
   return (
     <div>
-      
+      <h1>ALPs</h1>
+      {/* <p>Welcome, {name}!</p> */}
     </div>
   );
 }
