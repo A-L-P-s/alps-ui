f0e01a83433cac863539dceefd79599d8bf3aff7
Kirk Hauck <kirkihauck@gmail.com>
2023-05-15 17:08:19 -0600

    feat: Make form state controlled

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -20,7 +20,8 @@ const Form = () => {
   const [verb, setVerb] = useState<string>('');
   const [engVerb, setEngVerb] = useState<string>('');
   const [grammarPoints, setGrammarPoints] = useState<IGrammarPoint[]>([]);
-  const [sentences, setSentences] = useState<string[]>([]);
+  const [sent1, setSent1] = useState<string>('');
+  const [sent2, setSent2] = useState<string>('');
 
   useEffect(() => {
     //fetch new challenge someday
@@ -38,10 +39,22 @@ const Form = () => {
         <h2>{verb}</h2>
         <h3>{engVerb}</h3>
         <img src={infoIcon} alt='instructions icon' />
-        {grammarPoints.length && <label>{grammarPoints[0].grammar_point} | {grammarPoints[0].eng_grammar_point}</label>}
-        <input type='text' />
-        {grammarPoints.length && <label>{grammarPoints[1].grammar_point} | {grammarPoints[1].eng_grammar_point}</label>}
-        <input type='text' />
+        {grammarPoints.length && <label htmlFor='sent1'>{grammarPoints[0].grammar_point} | {grammarPoints[0].eng_grammar_point}</label>}
+        <input
+          id='sent1'
+          type='text'
+          value={sent1}
+          placeholder='Enter your sentence'
+          onChange={event => setSent1(event.target.value)}
+        />
+        {grammarPoints.length && <label htmlFor='sent2'>{grammarPoints[1].grammar_point} | {grammarPoints[1].eng_grammar_point}</label>}
+        <input
+          id='sent2'
+          type='text'
+          value={sent2}
+          placeholder='Enter your sentence'
+          onChange={event => setSent2(event.target.value)}
+        />
       </div>
     </form>
   );
