20221f37ca4bc54365ca43a29fb52ffa9f645333
Katherine Blaine <blaine.katherine12@gmail.com>
2023-05-16 14:54:33 -0600

    refac: Clean up syntax and delete unused CSS properties

--- src/components/ChallengeCard/ChallengeCard.tsx
+++ src/components/ChallengeCard/ChallengeCard.tsx
@@ -15,10 +15,8 @@ const ChallengeCard = ({ verb, eng_verb, image_url, image_alt_text, date }: IPro
       <img src={image_url} alt={image_alt_text}></img>
       <div className='card-right-content'>
         <h2>{date}</h2>
-        <div className='verb'>
-          <h3>{verb}</h3>
-          <p>({eng_verb})</p>
-        </div>
+        <h3>{verb}</h3>
+        <p>({eng_verb})</p>
       </div>
     </div>
   );
