1b9e2a84c6c321e29e281dd910afb1720090b99b
Barbie Ordonez <ordonezbarbie@gmail.com>
2023-05-15 18:56:57 -0600

    Pass user to home so that each user and language can be displayed

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,22 +1,18 @@
 import React from 'react';
 import './Home.css';
 import User from '../User/User';
+import users from '../../sampleData/users'
 
-
+const userOne = users.data[0].attributes;
+const userTwo = users.data[1].attributes;
 
 const Home = () => {
   return (
     <div>
       <p className='choose-user'>Choose your user</p>
       <div className='user-container'>
-        <div className='user'>
-          <h2>Deniz</h2>
-          <p>Turkish</p>
-        </div>
-        <div className='user'>
-          <h2>Alexis</h2>
-          <p>Spanish</p>
-        </div>
+        <User name={userOne.name} preferred_lang={userOne.preferred_lang}/>
+        <User name={userTwo.name} preferred_lang={userTwo.preferred_lang}/>
       </div>
     </div>
   );
--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -1,21 +1,16 @@
 import React from 'react';
 import './User.css';
-import users from '../../sampleData/users'
 
-const userOne = users.data[0].attributes;
-const userTwo = users.data[1].attributes;
+interface UserProps {
+  name:string,
+  preferred_lang:string
+}
 
-const User = () => {
+const User = (props: UserProps) => {
   return (
     <div>
-      <div>
-        <p>Name: {userOne.name}</p>
-        <p>{userOne.preferred_lang}</p>
-      </div>
-      <div>
-        <p>Name: {userTwo.name}</p>
-        <p>{userTwo.preferred_lang}</p>
-      </div>
+      <p>{props.name}</p>
+      <p>{props.preferred_lang}</p>
     </div>
   );
 }
