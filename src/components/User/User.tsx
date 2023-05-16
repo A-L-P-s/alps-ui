560d7208a60ec846502a6ba1d74ebd01a2b17117
Barbie Ordonez <ordonezbarbie@gmail.com>
2023-05-15 18:26:38 -0600

    Import user mock data to create user one and two for each language

--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -1,3 +1,23 @@
 import React from 'react';
 import './User.css';
 import users from '../../sampleData/users'
+
+const userOne = users.data[0].attributes;
+const userTwo = users.data[1].attributes;
+
+const User = () => {
+  return (
+    <div>
+      <div>
+        <p>Name: {userOne.name}</p>
+        <p>{userOne.preferred_lang}</p>
+      </div>
+      <div>
+        <p>Name: {userTwo.name}</p>
+        <p>{userTwo.preferred_lang}</p>
+      </div>
+    </div>
+  );
+}
+
+export default User;
