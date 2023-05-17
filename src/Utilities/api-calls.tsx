54e0adae5da966592a329886c61b63b290ab8fcd
Katherine Blaine <blaine.katherine12@gmail.com>
2023-05-16 18:13:54 -0600

    feat: Add initial structure to fetch specific challenge feedback data

--- src/Utilities/api-calls.tsx
+++ src/Utilities/api-calls.tsx
@@ -17,4 +17,18 @@ const getUsers: (userId?: string) => void = async (userId) => {
     });
 }
 
-export default getUsers;
+const getFeedback: (challengeId?: string, userId?:string) => void = async (challengeId, userId) => {
+  return fetch(`https://5178589b-c8d7-4c00-ae22-57e3d6493139.mock.pstmn.io/api/v1/users/${userId}/challenges/${challengeId}`)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`${response.status}: ${response.statusText}`);
+    } else {
+      return response.json();
+    }
+  })
+  .catch(error => {
+    console.error('An error occurred:', error);
+  });
+}
+
+export default { getUsers, getFeedback }
