627039a0cb96faea44e2a13af8ddfc9d80908da1
Kirk Hauck <kirkihauck@gmail.com>
2023-05-16 19:39:36 -0600

    refac: Change instructions to confirm the user does not need to write about the picture

--- src/components/Instructions/Instructions.tsx
+++ src/components/Instructions/Instructions.tsx
@@ -6,7 +6,7 @@ const Instructions = () => {
     <div>
       <h2>Let's Summit this Challenge!</h2>
       <p>The best way to retain a language is to practice your words, and a picture is worth a thousand! We're giving you a picture, a verb, and some grammar points to get you started.</p>
-      <p>Challenge your knowledge and flex those creative muscles by writing sentences about the picture. For each text area, write a sentence that is about the image using the provided verb in a way that conforms to the rules of the grammar point.</p>
+      <p>Challenge your knowledge and flex those creative muscles by writing sentences. For each text area, write a sentence using the provided verb in a way that conforms to the rules of the grammar point. The picture is just there to help get you started, and your sentence doesn't have to be about the picture. Just make sure you are using the word correctly in the sentence.</p>
       <p>Once you're done, click "Submit" and brace yourself for immediate feedback!</p>
     </div>
   );
