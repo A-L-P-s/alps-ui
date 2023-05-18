90d3b7c2643f5af5ef4b6385d074366212b94f94
Barbie Ordonez <ordonezbarbie@gmail.com>
2023-05-18 11:40:42 -0600

    Feature: Add feeback fetch request

--- src/components/Feedback/Feedback.tsx
+++ src/components/Feedback/Feedback.tsx
@@ -5,49 +5,60 @@ import './Feedback.css';
 import { getFeedback } from '../../Utilities/api-calls';
 import { IFeedback } from '../../Utilities/interfaces';
 
+// const initialFeedback = {
+//   data: []
+// }
 
 const Feedback = () => {
-  //const [feedback, setFeedback] = useState<IFeedback>(null);
-  const [imgUrl, setImgUrl] = useState<string>('');
-  const [imgAlt, setImgAlt] = useState<string>('')
-  const [verb, setVerb] = useState<string>('');
-  const [engVerb, setEngVerb] = useState<string>('');
-  const [sentences, setSentences] = useState<ISentences[]>([]);
+  const [feedback, setFeedback] = useState<IFeedback|undefined>();
+  // const [imgUrl, setImgUrl] = useState<string>('');
+  // const [imgAlt, setImgAlt] = useState<string>('')
+  // const [verb, setVerb] = useState<string>('');
+  // const [engVerb, setEngVerb] = useState<string>('');
+  // const [sentences, setSentences] = useState<ISentences[]>([]);
 
   useEffect(() => {
+    //get user by name from url
+    //pull the challenge id
+
+
+
     //fetch goes here someday
     getFeedback("1", "55")
-      .then( feedback => {
-        console.log(feedback)
+      .then( feedbackData => {
+        console.log(feedbackData)
 
-        setImgUrl( feedback.)
+        setFeedback(feedbackData);
       })
       
-    setImgUrl(mockData.image_url);
-    setImgAlt(mockData.image_alt_text);
-    setVerb(mockData.verb);
-    setEngVerb(mockData.eng_verb);
-    setSentences(mockData.sentences);
+    // setImgUrl(mockData.image_url);
+    // setImgAlt(mockData.image_alt_text);
+    // setVerb(mockData.verb);
+    // setEngVerb(mockData.eng_verb);
+    // setSentences(mockData.sentences);
   }, []);
 
   return (
+    feedback ?
     <div className='feedback-container'>
-      <img src={imgUrl} alt={imgAlt} className='prompt-img'/>
-      {sentences.length && <div className='sentences-container'>
-        <h2>{verb}</h2>
-        <h3>{engVerb}</h3>
-        <h4>{sentences[0].grammar_point} | {sentences[0].eng_grammar_point}</h4>
+      <img src={feedback.data.attributes.image_url} alt={feedback.data.attributes.image_alt_text} className='prompt-img'/>
+      {feedback.data.attributes.sentences.length && <div className='sentences-container'>
+        <h2>{feedback.data.attributes.verb}</h2>
+        <h3>{feedback.data.attributes.eng_verb}</h3>
+        <h4>{feedback.data.attributes.sentences[0].grammar_point} | {feedback.data.attributes.sentences[0].eng_grammar_point}</h4>
         <h5>Your Sentence</h5>
-        <p>{sentences[0].user_sent}</p>
+        <p>{feedback.data.attributes.sentences[0].user_sent}</p>
         <h5>Corrected Sentence</h5>
-        <p>{sentences[0].ai_sent}</p>
-        <p>{sentences[0].ai_explanation}</p>
+        <p>{feedback.data.attributes.sentences[0].ai_sent}</p>
+        <p>{feedback.data.attributes.sentences[0].ai_explanation}</p>
         {/* Will need to make the username dynamic */}
         <Link to={'/Deniz/dashboard'}>
           <button>Home</button>
         </Link>
       </div>}
     </div>
+    :
+    <></>
   );
 }
 
