2fe89a8350da798f12f6710921d56dfd5db5af15
Barbie Ordonez <ordonezbarbie@gmail.com>
2023-05-18 21:32:05 -0600

    Refact: Delete console logs, commented out code

--- src/components/Feedback/Feedback.tsx
+++ src/components/Feedback/Feedback.tsx
@@ -8,34 +8,20 @@ import { IFeedback } from '../../Utilities/interfaces';
 import { useParams } from 'react-router-dom';
 import { getUsers } from '../../Utilities/api-calls';
 
-// const initialFeedback = {
-//   data: []
-// }
 
 const Feedback = () => {
   const [feedback, setFeedback] = useState<IFeedback|undefined>();
-  // const [imgUrl, setImgUrl] = useState<string>('');
-  // const [imgAlt, setImgAlt] = useState<string>('')
-  // const [verb, setVerb] = useState<string>('');
-  // const [engVerb, setEngVerb] = useState<string>('');
-  // const [sentences, setSentences] = useState<ISentences[]>([]);
-
-  const { userName, challengeId } = useParams()
+ 
+  const { userName, challengeId } = useParams();
 
   const [userId, setUserId] = useState<string>('');
 
   useEffect(() => {
-    //get user id from the user name
     getUsers()
       .then( users => {
         const matchingUser = users?.data.find(user => user.attributes.name == userName)
         setUserId(matchingUser?.id || "")
       })
-    // setImgUrl(mockData.image_url);
-    // setImgAlt(mockData.image_alt_text);
-    // setVerb(mockData.verb);
-    // setEngVerb(mockData.eng_verb);
-    // setSentences(mockData.sentences);
   }, []);
 
   useEffect(() => {
@@ -43,7 +29,7 @@ const Feedback = () => {
       .then( feedbackData => {
         setFeedback(feedbackData);
       })
-  }, [userId])
+  }, [userId]);
 
   return (
     feedback ?
@@ -58,7 +44,6 @@ const Feedback = () => {
         <h5>Corrected Sentence</h5>
         <p>{feedback.data.attributes.sentences[0].ai_sent}</p>
         <p>{feedback.data.attributes.sentences[0].ai_explanation}</p>
-        {/* Will need to make the username dynamic */}
         <Link to={`/${userName}/dashboard`}>
           <button>Home</button>
         </Link>
