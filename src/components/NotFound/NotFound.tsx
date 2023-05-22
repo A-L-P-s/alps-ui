82f07a72937fd96a8005c07849b42fbca1cc4ad7
Katherine Blaine <blaine.katherine12@gmail.com>
2023-05-22 11:14:15 -0500

    style: Add css and basic layout for NotFound component

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
+import './NotFound.css'
 
 const NotFound = () => {
   return (
-    <>
+    <div className='not-found'>
       <h1>Uh oh!</h1>
-      <h3>It looks like the page you visited no longer exists. Please return home and try again!</h3>
+      <h3>It looks like this page no longer exists. Please return home and try again!</h3>
       <Link to='/'><button>Home</button></Link>
-    </>
+    </div>
   )
 }
 
