47897dfa4d2202aaefc84d5949a6c4a307e97c74
Kirk Hauck <kirkihauck@gmail.com>
2023-05-22 10:33:49 -0600

    setup: Intercept users, dashboards, and past feedback

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,7 +1,29 @@
 /// <reference types="Cypress" />
 
 describe('template spec', () => {
+  beforeEach('visit and intercept home', () => {
+    cy.interceptUsers();
+    cy.interceptDashboard_1();
+    cy.interceptDashboard_55();
+    cy.interceptPastFeedback_1_1();
+    cy.interceptPastFeedback_55_3();
+  });
+
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+    cy.visit('http://localhost:3000');
+
+    cy.get('button').eq(0).click();
+
+    cy.get('[href="/55/feedback/3"]').click();
+
+    cy.get('button').click();
+
+    cy.get('h1').click();
+
+    cy.get('button').eq(1).click();
+
+    cy.get('[href="/1/feedback/1"]').click();
+
+    cy.get('button').click();
+  });
+});
