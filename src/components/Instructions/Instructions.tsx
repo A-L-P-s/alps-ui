8e42e3ee9b21f15d44c05b34060b2a25f76c570b
Barbie Ordonez <ordonezbarbie@gmail.com>
2023-05-22 14:23:40 -0600

    Style: Change color scheme for instruction modal

--- src/components/Instructions/Instructions.tsx
+++ src/components/Instructions/Instructions.tsx
@@ -3,7 +3,7 @@ import './Instructions.css';
 
 const Instructions = () => {
   return (
-    <div>
+    <div className='instructions'>
       <h2>Let's Summit this Challenge!</h2>
       <p>The best way to retain a language is to practice your words, and a picture is worth a thousand! We're giving you a picture, a verb, and some grammar points to get you started.</p>
       <p>Challenge your knowledge and flex those creative muscles by writing sentences about the picture. For each text area, write a sentence that is about the image using the provided verb in a way that conforms to the rules of the grammar point.</p>
