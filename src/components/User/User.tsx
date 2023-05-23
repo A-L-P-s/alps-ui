e6a630ccf62565531e9896bda98cd327cb294151
Katherine Blaine <blaine.katherine12@gmail.com>
2023-05-23 15:47:50 -0500

    feat: Add avatar files and variable to user component

--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -1,4 +1,6 @@
 import { Link } from 'react-router-dom';
+import panda from '../../assets/panda.png'
+import dog from '../../assets/dog.png'
 import './User.css';
 
 interface IProps {
@@ -9,10 +11,13 @@ interface IProps {
 
 const User = ({ name, preferred_lang, id }: IProps): JSX.Element => {
 
+  const avatar = name === 'Deniz' ? panda : dog
+
   return (
     <div className='user'>
+      <img src={avatar} alt={`${avatar} icon`} className='avatar-icons'></img>
       <p className='user-name'>{name}</p>
-      <p>{preferred_lang}</p>
+      <p>{`Practice ${preferred_lang}!`}</p>
       <Link to={`/${id}/dashboard`}><button>{`Continue as ${name}`}</button></Link>
     </div>
   );
