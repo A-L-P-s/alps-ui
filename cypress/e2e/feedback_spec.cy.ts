15c78bc236294c5de2f30cb3eeb958df3488e004
Barbie Ordonez <ordonezbarbie@gmail.com>
2023-05-24 12:23:57 -0600

    Test: Test the feedback view

--- cypress/e2e/feedback_spec.cy.ts
+++ cypress/e2e/feedback_spec.cy.ts
@@ -11,4 +11,25 @@ describe ('Feedback flow', () => {
     cy.get('.challenge-card-container').should('be.visible');
   });
 
+  it ('should render past feedback with all elements', () => {
+    cy.get(':nth-child(1) > a > button')
+      .first()
+      .click();
+ 
+    cy.get('.challenge-card-container').should('be.visible') ;
+    cy.get('[href="/1/feedback/100"] > .challenge-card > .card-right-content')
+      .first()
+      .click();
+ 
+    cy.get('.feedback-container') 
+      .should('contain', 'Your challenge from 05/23/2023')
+      .and('contain', 'Sentence #1')
+      .and('contain', 'presente ✴ simple present tense')
+      .and('contain', 'Your response')
+      .and('contain', 'Él habla español y francés con fluidez.')
+      .and('contain', 'Corrected Sentence')
+      .and('contain', 'Él habla español y francés con fluidez.')
+      .and('contain', 'Feedback')
+      .and('contain', 'This sentence is correct. Nice work rockstar!');
+    })
 })
