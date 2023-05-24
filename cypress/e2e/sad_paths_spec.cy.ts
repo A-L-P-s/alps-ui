298b97b52aba530fe4f39b484e8c83e1c7e27b71
Kirk Hauck <kirkihauck@gmail.com>
2023-05-24 14:30:20 -0600

    cypress: Add sad path tests for dashboard and past feedback

--- cypress/e2e/sad_paths_spec.cy.ts
+++ cypress/e2e/sad_paths_spec.cy.ts
@@ -1,9 +1,34 @@
 describe('sad path tests', () => {
   beforeEach(() => {
     cy.interceptAll();
+    cy.visit('http://localhost:3000');
   });
 
-  it('passes', () => {
-    cy.visit('http://localhost:3000');
+  it('should show an error if the user navigates to their dashboard with a bad request and allow them to return Home', () => {
+    cy.intercept404Dashboard_1();
+
+    cy.get('button').first().click();
+
+    cy.get('h3').contains('Error: 404: Not Found')
+      .get('h4').contains('Here\'s a handy button to return Home!')
+      .get('button').contains('Home').click()
+
+    cy.url().should('eq', 'http://localhost:3000/')
+      .get('.user').should('have.length', 2);
+  });
+
+  it('should show an error if the user selects a past feedback with a bad request and allow them to return Home', () => {
+    cy.intercept404PastFeedback_1_1();
+
+    cy.get('button').first().click();
+
+    cy.get('.challenge-card').click();
+
+    cy.get('h3').contains('Error: 404: Not Found')
+      .get('h4').contains('Here\'s a handy button to return Home!')
+      .get('button').contains('Home').click()
+
+    cy.url().should('eq', 'http://localhost:3000/')
+      .get('.user').should('have.length', 2);
   });
 });
